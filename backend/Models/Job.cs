ea8bf53ae88198c691e5f73c1c1880d352f9046f
mandeepkumarsalhan <mandeepkumarsalhan@gmail.com>
2026-05-12 12:48:46 +0200

    Model class added 5th commit

---
+++ backend/Models/Job.cs
@@ -0,0 +1,10 @@
+namespace backend.Models;
+
+public class Job
+{
+    public int Id {get;set;} = string.Empty;
+    public string Company { get; set; } = string.Empty;
+    public string Location { get; set; } = string.Empty;
+    public string Description { get; set; } = string.Empty;
+    public DateTime PostedDate { get; set; } = DateTime.UtcNow;
+}
