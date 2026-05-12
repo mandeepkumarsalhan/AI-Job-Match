e0ed2f4ce889a9de22c302fc19ed288f243a15dc
mandeepkumarsalhan <mandeepkumarsalhan@gmail.com>
2026-05-12 12:29:28 +0200

    added dotnet  dev certs https --trust, 2nd commit

--- backend/Program.cs
+++ backend/Program.cs
@@ -14,6 +14,7 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+
 var summaries = new[]
 {
     "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
@@ -32,7 +33,9 @@ app.MapGet("/weatherforecast", () =>
     return forecast;
 })
 .WithName("GetWeatherForecast");
-
+app.MapGet("/", () => {
+    return "Welcome to AI JOB Match";
+});
 app.Run();
 
 record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
