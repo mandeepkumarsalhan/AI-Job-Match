5f6310253ff6067db2a1beac8dd371a3dbd770e0
mandeepkumarsalhan <mandeepkumarsalhan@gmail.com>
2026-05-24 22:17:58 +0200

    Add Filtering (Search+ City filter)

--- backend/Controllers/NavJobsController.cs
+++ backend/Controllers/NavJobsController.cs
@@ -15,9 +15,21 @@ public class NavJobsController : ControllerBase
     }
 
     [HttpGet]
-    public async Task<IActionResult> Get()
+    public async Task<IActionResult> Get([FromQuery] string? city,[FromQuery] string? search)
     {
         var jobs = await _navApiService.GetJobsAsync();
+        if(!string.IsNullOrWhiteSpace(city))
+        {
+            jobs = jobs.Where(j=> j.Location != null &&
+            j.Location.Contains(city, StringComparison.OrdinalIgnoreCase)
+            ).ToList();
+        }
+        if(!string.IsNullOrWhiteSpace(search))
+        {
+            jobs = jobs.Where(j=> j.Title != null &&
+            j.Title.Contains(search, StringComparison.OrdinalIgnoreCase)
+            ).ToList();
+        }
         return Ok(jobs);
     }
 }
